346ea94925ba2ab6182cb663fe6828472542b0c5
mDalessandro <mattd942@gmail.com>
2018-09-07 19:02:32 -0400

    feat: add social media links

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -3,9 +3,15 @@ import Photo from './Photo';
 
 const Social = () => (
   <div className="social">
-    <Photo source="../../facebook.png" alt="Facebook Link" />
-    <Photo source="../../instagram.png" alt="Instagram Link" />
-    <Photo source="../../yelp.png" alt="Yelp Link" />
+    <a className="social-link" href="https://www.facebook.com/yumegaarukara/">
+      <Photo source="../../facebook.png" alt="Facebook Link" />
+    </a>
+    <a className="social-link" href="https://www.instagram.com/yumegaarukara/?hl=en">
+      <Photo source="../../instagram.png" alt="Instagram Link" />
+    </a>
+    <a className="social-link" href="https://www.yelp.com/biz/yume-ga-arukara-cambridge-2">
+      <Photo source="../../yelp.png" alt="Yelp Link" />
+    </a>
   </div>
 );
 
