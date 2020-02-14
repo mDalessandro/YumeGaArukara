90ae8ba0d2f150d463cfe06150d5d55a43fdd702
mDalessandro <mattd942@gmail.com>
2020-02-13 21:23:16 -0500

    feat: update hours

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -1,46 +1,45 @@
 import React from 'react';
 
 const Hours = () => {
-  const lunchHours = '11am - 2pm';
-  const sundayLunchHours = '11am - 3pm';
-  const dinnerHours = '5pm - 9pm';
+  const hours = '11am - 2pm + 5pm - 9pm';
+  const sundayHours = '11am - sold out';
 
   return (
     <div className="hours">
       <h2>Hours</h2>
       <div id="Sunday" className="dateTime">
         <div className="day">Sunday</div>
-        <div className="time">{sundayLunchHours}</div>
+        <div className="time">{sundayHours}</div>
       </div>
       <br />
       <div id="Monday" className="dateTime">
         <div className="day">Monday</div>
-        <div className="time">{dinnerHours}</div>
+        <div className="time">{hours}</div>
       </div>
       <br />
       <div id="Tuesday" className="dateTime">
         <div className="day">Tuesday</div>
-        <div className="time">{`${lunchHours} + ${dinnerHours}`}</div>
+        <div className="time">{hours}</div>
       </div>
       <br />
       <div id="Wednesday" className="dateTime">
         <div className="day">Wednesday</div>
-        <div className="time">{`${lunchHours} + ${dinnerHours}`}</div>
+        <div className="time">{hours}</div>
       </div>
       <br />
       <div id="Thursday" className="dateTime">
         <div className="day">Thursday</div>
-        <div className="time">{`${lunchHours} + ${dinnerHours}`}</div>
+        <div className="time">{hours}</div>
       </div>
       <br />
       <div id="Friday" className="dateTime">
         <div className="day">Friday</div>
-        <div className="time">{`${lunchHours} + ${dinnerHours}`}</div>
+        <div className="time">{hours}</div>
       </div>
       <br />
       <div id="Saturday" className="dateTime">
         <div className="day">Saturday</div>
-        <div className="time">{`${lunchHours} + ${dinnerHours}`}</div>
+        <div className="time">{hours}</div>
       </div>
     </div>
   );
