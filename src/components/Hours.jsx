8af489f6ec2c8d35f06efd8ed4509e05a6c94c3d
mDalessandro <mattd942@gmail.com>
2020-03-18 23:05:13 -0400

    feat: update hours for takeout only

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -1,45 +1,46 @@
 import React from 'react';
 
 const Hours = () => {
-  const hours = '11am - 2pm + 5pm - 9pm';
-  const sundayHours = '11am - sold out';
+  const takeOutHours = '5pm - 9pm';
 
   return (
     <div className="hours">
-      <h2>Hours</h2>
+      <h2>Hours (Take-out only*)</h2>
+      <span>* To order, please DM on Instagram (@yumegaarukara) or walk up.</span>
+      <div className="lastOrderTime">Last order at 8:50pm.</div>
       <div id="Sunday" className="dateTime">
         <div className="day">Sunday</div>
-        <div className="time">{sundayHours}</div>
+        <div className="time">{takeOutHours}</div>
       </div>
       <br />
       <div id="Monday" className="dateTime">
         <div className="day">Monday</div>
-        <div className="time">{hours}</div>
+        <div className="time">Closed</div>
       </div>
       <br />
       <div id="Tuesday" className="dateTime">
         <div className="day">Tuesday</div>
-        <div className="time">{hours}</div>
+        <div className="time">Closed</div>
       </div>
       <br />
       <div id="Wednesday" className="dateTime">
         <div className="day">Wednesday</div>
-        <div className="time">{hours}</div>
+        <div className="time">{takeOutHours}</div>
       </div>
       <br />
       <div id="Thursday" className="dateTime">
         <div className="day">Thursday</div>
-        <div className="time">{hours}</div>
+        <div className="time">{takeOutHours}</div>
       </div>
       <br />
       <div id="Friday" className="dateTime">
         <div className="day">Friday</div>
-        <div className="time">{hours}</div>
+        <div className="time">{takeOutHours}</div>
       </div>
       <br />
       <div id="Saturday" className="dateTime">
         <div className="day">Saturday</div>
-        <div className="time">{hours}</div>
+        <div className="time">{takeOutHours}</div>
       </div>
     </div>
   );
