fec7dee314a9e455be58157f5322cf945521bb22
mDalessandro <mattd942@gmail.com>
2020-03-28 15:02:18 -0400

    fix: update hours subtitle

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -4,43 +4,50 @@ const Hours = () => {
   const takeOutHours = '4pm - 8pm';
 
   return (
-    <div className="hours">
+    <div className="hours-wrapper">
       <h2>Hours (Take-out only*)</h2>
-      <span>* To order, please DM on Instagram (@yumegaarukara) or walk up.</span>
-      <div className="lastOrderTime">Last order at 7:50pm.</div>
-      <div id="Sunday" className="dateTime">
-        <div className="day">Sunday</div>
-        <div className="time">{takeOutHours}</div>
+      <div className="hours-subtitle">
+        * Udon Pre-order: Begins daily at 9am at&nbsp;
+        <a href="https://tinyurl.com/YumeOrder" target="_blank" rel="noreferrer noopener">
+          tinyurl.com/YumeOrder
+        </a>
+        . Closes when we sell out. Last order at 7:50pm.
       </div>
-      <br />
-      <div id="Monday" className="dateTime">
-        <div className="day">Monday</div>
-        <div className="time">Closed</div>
-      </div>
-      <br />
-      <div id="Tuesday" className="dateTime">
-        <div className="day">Tuesday</div>
-        <div className="time">Closed</div>
-      </div>
-      <br />
-      <div id="Wednesday" className="dateTime">
-        <div className="day">Wednesday</div>
-        <div className="time">{takeOutHours}</div>
-      </div>
-      <br />
-      <div id="Thursday" className="dateTime">
-        <div className="day">Thursday</div>
-        <div className="time">{takeOutHours}</div>
-      </div>
-      <br />
-      <div id="Friday" className="dateTime">
-        <div className="day">Friday</div>
-        <div className="time">{takeOutHours}</div>
-      </div>
-      <br />
-      <div id="Saturday" className="dateTime">
-        <div className="day">Saturday</div>
-        <div className="time">{takeOutHours}</div>
+      <div className="hours">
+        <div id="Sunday" className="dateTime">
+          <div className="day">Sunday</div>
+          <div className="time">{takeOutHours}</div>
+        </div>
+        <br />
+        <div id="Monday" className="dateTime">
+          <div className="day">Monday</div>
+          <div className="time">Closed</div>
+        </div>
+        <br />
+        <div id="Tuesday" className="dateTime">
+          <div className="day">Tuesday</div>
+          <div className="time">Closed</div>
+        </div>
+        <br />
+        <div id="Wednesday" className="dateTime">
+          <div className="day">Wednesday</div>
+          <div className="time">{takeOutHours}</div>
+        </div>
+        <br />
+        <div id="Thursday" className="dateTime">
+          <div className="day">Thursday</div>
+          <div className="time">{takeOutHours}</div>
+        </div>
+        <br />
+        <div id="Friday" className="dateTime">
+          <div className="day">Friday</div>
+          <div className="time">{takeOutHours}</div>
+        </div>
+        <br />
+        <div id="Saturday" className="dateTime">
+          <div className="day">Saturday</div>
+          <div className="time">{takeOutHours}</div>
+        </div>
       </div>
     </div>
   );
